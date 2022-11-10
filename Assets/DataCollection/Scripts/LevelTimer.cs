9698c430a0b931d3372c3cd01583f7d27cfead32
Brandon Lyman <lyman.br@northeastern.edu>
2022-11-10 12:31:48 -0500

    Level time data log
    + Time spent in level is now logged to the "TimeToCompleteLevel" variable

---
+++ Assets/DataCollection/Scripts/LevelTimer.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+public class LevelTimer : MonoBehaviour
+{
+    [SerializeField] private FloatVariable _levelTimerVariable;
+
+    private bool _timerRunning = false;
+    
+    #region MonoBehaviour Methods
+    private void Start()
+    {
+        _levelTimerVariable.Value = 0.0f;
+        _timerRunning = true;
+    }
+
+    private void Update()
+    {
+        if (_timerRunning)
+        {
+            _levelTimerVariable.Value += Time.deltaTime;
+        }
+    }
+    #endregion
+    
+    /// <summary>
+    /// Stops the level timer.
+    /// </summary>
+    public void StopTimer()
+    {
+        _timerRunning = false;
+    }
+}
